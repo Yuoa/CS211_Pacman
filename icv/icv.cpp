f1fcb401bf7c333ae77212d6d64abcb023ffa571
Jio Gim <with@labus.io>
2017-12-19 23:12:23 +0900

    opencv test

---
+++ icv/icv.cpp
@@ -0,0 +1,16 @@
+//By labus YUOA, 2017.
+#include <iostream>
+#include <opencv2/core.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int c, char** v) {
+
+	Mat *temp = new Mat();
+
+	cout << "hello. test. creating matrix is success." << endl;
+
+	return 0;	
+
+}
